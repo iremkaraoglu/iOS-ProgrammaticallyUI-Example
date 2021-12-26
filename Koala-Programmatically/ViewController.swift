4a06998f332f5c29621d4e2868906c8e0f06f948
iremkaraoglu <iremk97@gmail.com>
2021-12-26 18:15:48 +0300

    add koala image to the screen

--- Koala-Programmatically/ViewController.swift
+++ Koala-Programmatically/ViewController.swift
@@ -12,6 +12,18 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let koalaView = UIImageView()
+        koalaView.image = UIImage(named: "koala")
+        koalaView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(koalaView)
+        
+        NSLayoutConstraint.activate([
+            koalaView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            koalaView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            koalaView.widthAnchor.constraint(equalToConstant: 320),
+            koalaView.heightAnchor.constraint(equalToConstant: 320)
+        ])
+        
     }
 
 
