de0c84c36c2dd374062d8ce4b7f486fc42890924
iremkaraoglu <iremk97@gmail.com>
2022-01-20 21:10:20 +0300

    add koala text

--- Koala-Programmatically/ViewController.swift
+++ Koala-Programmatically/ViewController.swift
@@ -12,6 +12,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .white
+        
         let koalaView = UIImageView()
         koalaView.image = UIImage(named: "koala")
         koalaView.translatesAutoresizingMaskIntoConstraints = false
@@ -22,6 +25,17 @@ class ViewController: UIViewController {
             koalaView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             koalaView.widthAnchor.constraint(equalToConstant: 320),
             koalaView.heightAnchor.constraint(equalToConstant: 320)
+        ])
+        
+        let koalaText = UILabel()
+        koalaText.text = "I love koalas ❤️"
+        koalaText.font = UIFont.systemFont(ofSize: 42)
+        koalaText.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(koalaText)
+        
+        NSLayoutConstraint.activate([
+            koalaText.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            koalaText.topAnchor.constraint(equalTo: koalaView.bottomAnchor, constant: 64)
         ])
         
     }
